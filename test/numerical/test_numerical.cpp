060952af635f6c7a3b8c6436045d76ba8051af9e
BackRaw <backraw@gmx.de>
2019-08-10 20:26:00 +0200

    test/numerical: Combine test_int and test_float inside test_numerical.cpp
    Signed-off-by: BackRaw <backraw@gmx.de>

--- test/numerical/test_numerical.cpp
+++ test/numerical/test_numerical.cpp
@@ -8,6 +8,10 @@
 #include "test_numerical.h"
 
 
+//
+// int
+//
+
 TEST(TestInt, Required)
 {
     cppargparse::test::numerical_required<int>();
@@ -17,4 +21,20 @@ TEST(TestInt, Required)
 TEST(TestInt, Optional)
 {
     cppargparse::test::numerical_optional<int>();
+}
+
+
+//
+// float
+//
+
+TEST(TestFloat, Required)
+{
+    cppargparse::test::numerical_required<float>();
+}
+
+
+TEST(TestFloat, Optional)
+{
+    cppargparse::test::numerical_optional<float>();
 }
