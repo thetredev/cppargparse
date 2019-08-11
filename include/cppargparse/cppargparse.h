a13d77c70b71d8d10b10166091ffb1e1d6b89e4a
BackRaw <backraw@gmx.de>
2019-08-11 07:38:16 +0200

    include/cppargparse.h: Change include order
    Signed-off-by: BackRaw <backraw@gmx.de>

--- include/cppargparse/cppargparse.h
+++ include/cppargparse/cppargparse.h
@@ -1,10 +1,10 @@
 #ifndef CPPARGPARSE_H
 #define CPPARGPARSE_H
 
+#include <cppargparse/types.h>
+#include <cppargparse/globals.h>
 #include <cppargparse/algorithm.h>
 #include <cppargparse/arguments.h>
-#include <cppargparse/globals.h>
 #include <cppargparse/parser.h>
-#include <cppargparse/types.h>
 
 #endif // CPPARGPARSE_H
