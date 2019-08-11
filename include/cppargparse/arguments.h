e773d872bd6291a107493077b64fbc246896a660
BackRaw <backraw@gmx.de>
2019-08-11 02:57:44 +0200

    Add missing library include for double.h
    Signed-off-by: BackRaw <backraw@gmx.de>

--- include/cppargparse/arguments.h
+++ include/cppargparse/arguments.h
@@ -2,6 +2,7 @@
 #define CPPARGPARSE_ARGUMENTS_H
 
 #include <cppargparse/arguments/default.h>
+#include <cppargparse/arguments/double.h>
 #include <cppargparse/arguments/float.h>
 #include <cppargparse/arguments/int.h>
 #include <cppargparse/arguments/string.h>
