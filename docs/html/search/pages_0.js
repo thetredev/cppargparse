cab8b5f278f48f954c98c84a22b2838642f5c5cc
Timo Reichl <timo.reichl@corscience.de>
2019-08-21 12:12:39 +0200

    Update source code documentation
    Signed-off-by: Timo Reichl <timo.reichl@corscience.de>

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['c_2b_2b_20argparse_3a_20command_20line_20parsing_20made_20simple',['C++ ArgParse: command line parsing made simple',['../index.html',1,'']]],
-  ['c_2b_2b_20argparse_3a_20command_20line_20parsing_20made_20simple',['C++ ArgParse: command line parsing made simple',['../md_README.html',1,'']]]
+  ['c_2b_2b_20argparse_3a_20command_20line_20parsing_20made_20simple',['C++ ArgParse: command line parsing made simple',['../index.html',1,'']]]
 ];
